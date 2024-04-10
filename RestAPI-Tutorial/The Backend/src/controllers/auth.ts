85755b520e084fcd9c03993062181ae6e8083385
fadymaher24 <fady.maher2024@gmail.com>
2024-04-10 21:43:19 +0200

    fix a bug

--- RestAPI-Tutorial/The Backend/src/controllers/auth.ts
+++ RestAPI-Tutorial/The Backend/src/controllers/auth.ts
@@ -12,65 +12,6 @@ interface SignUpRequestBody {
   password: string;
 }
 
-export const signup = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const error = new ValidationError("Validation failed.", errors.array());
-      error.statusCode = 422;
-      throw error;
-    }
-
-    const { email, name, password } = req.body as SignUpRequestBody;
-
-    const hashedPw = bcrypt.hashSync(password, 12);
-    const user = new User({ email, password: hashedPw, name });
-
-    const result = await user.save();
-
-    res.status(201).json({ message: "User created!", userId: result._id });
-  } catch (err) {
-    next(err);
-  }
-};
-
-export const login = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user) {
-      throw new CustomError("A user with this email could not be found.", 401);
-    }
-
-    const loadedUser = user;
-    const isEqual = bcrypt.compareSync(password, user.password);
-    if (!isEqual) {
-      throw new CustomError("Wrong password!", 401);
-    }
-    const token = jwt.sign(
-      {
-        email: loadedUser.email,
-        userId: loadedUser._id.toString(),
-      },
-      "somesupersecretsecret",
-      { expiresIn: "1h" }
-    );
-
-    res.status(200).json({ token, userId: loadedUser._id.toString() });
-  } catch (err) {
-    next(err);
-  }
-};
-
 class ValidationError extends Error {
   data: any;
   statusCode: number;
@@ -90,6 +31,73 @@ class CustomError extends Error {
     this.statusCode = statusCode;
   }
 }
+
+export const signup = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new ValidationError("Validation failed.", errors.array());
+    error.statusCode = 422;
+    throw error;
+  }
+  const { email, name, password } = req.body as SignUpRequestBody;
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPw) => {
+      const user = new User({
+        email: email,
+        password: hashedPw,
+        name: name,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({ message: "User created!", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+
+  try {
+    loadedUser = await User.findOne({ email: email });
+
+    if (!loadedUser || !loadedUser.password) {
+      throw new CustomError("User not found or password is undefined", 401);
+    }
+    const isEqual = bcrypt.compare(password, loadedUser.password);
+    if (!isEqual) {
+      throw new CustomError("Wrong password!", 401);
+    }
+
+    const token = jwt.sign(
+      {
+        email: loadedUser?.email,
+        userId: loadedUser?._id.toString(),
+      },
+      "somesupersecretsecret",
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ token, userId: loadedUser?._id.toString() }); // Add null check here
+  } catch (err: any) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
 
 // Error handling middleware
 export const errorHandler = (
