62e1504649a5fb4e84c359ee6a2c35704122aea1
fadymaher24 <fady.maher2024@gmail.com>
2024-04-10 17:48:43 +0200

    .

---
+++ RestAPI-Tutorial/The Backend/src/models/user.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { Schema, Document } from "mongoose";
+
+interface IUser extends Document {
+  email: string;
+  password: string;
+  name: string;
+  status: string;
+  posts: string[];
+}
+
+const userSchema: Schema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+});
+
+export default mongoose.model<IUser>("User", userSchema);
+
+// Path: src/models/post.ts
