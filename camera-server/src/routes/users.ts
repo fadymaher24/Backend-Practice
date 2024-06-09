da733023c7e92948ec443557498e43e6b6d28136
Fady Maher <80794740+fadymaher24@users.noreply.github.com>
2024-06-09 22:05:19 +0300

    Delete camera-server directory

--- camera-server/src/routes/users.ts
+++
@@ -1,9 +0,0 @@
-import {Router, Request, Response}  from 'express';
-
-let router = Router();
-/* GET users listing. */
-router.get('/', function(req: Request, res: Response, next) {
-  res.send('respond with a resource');
-});
-
-export const users: Router = router;
--- camera-server/web/src/config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const isProduction = false;
-
-export const api = isProduction
-  ? "http://kemettech.com"
-  : "http://localhost:3001";
